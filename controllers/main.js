091520a50e038617a4f850e9c0080ac029270ab3
jebuAsus <95964955+Carbrex@users.noreply.github.com>
2023-01-17 01:49:38 +0530

    06.12.20 05-JWT-Basics wip

---
+++ controllers/main.js
@@ -0,0 +1,39 @@
+// check username, password in post(login) request
+// if exists create new JWT
+// send back to frontend
+
+// setup authentication so only the request with JWT can access the dashboard
+
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+    // we can use these ways to check for valid values
+    // mongoose validation
+    // joi
+    // check in the controller
+    if (!(username || password)) {
+        throw new CustomAPIError('Please provide email and password', 400);
+    }
+    // just for demo normally provided by db
+    const id = new Date().getDate();
+
+    // try to keep payload small, better experience for users
+    // just for demo, in production use long, complex and unguessable string value!!!!!!!!!
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {expiresIn:'30d'});
+
+    // console.log(username, password);
+    res.status(200).json({msg:'user created',token});
+}
+
+const dashboard = async (req, res) => {
+    // console.log(req.headers);
+    // console.log(req.user);
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({ msg: `Hello, ${req.user.username}`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}` });
+}
+
+module.exports = {
+    login, dashboard
+}
